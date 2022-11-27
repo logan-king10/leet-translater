3157cda637561c25766185b870b2c5a6e1dfa0e1
logan <95551215+logan-king10@users.noreply.github.com>
2022-11-27 12:27:28 -0600

    Add files via upload

---
+++ 1337.py
@@ -0,0 +1,81 @@
+import time
+# Online Python - IDE, Editor, Compiler, Interpreter
+a = 1
+
+
+
+def let():
+    a = input('first letter: ')
+    if a == "a":
+        print("@")
+    if a == "b":
+        print("6")
+    if a == "c":
+        print("(")
+    if a == "d":
+        print("[)")
+    if a == "e":
+        print("3")
+    if a == "f":
+        print("f")
+    if a == "g":
+        print("9")
+    if a == "h":
+        print("|-|")
+    if a == "i":
+        print("!")
+
+    if a == "j":
+        print("_|")
+    if a == "k":
+        print("|<")
+    if a == "l":
+        print("1")
+    if a == "m":
+        print("|\/|")
+    if a == "n":
+        print("|\|")
+    if a == "o":
+        print("0")
+    if a == "P":
+        print("|*")
+    if a == "Q":
+        print("()_")
+    if a == "R":
+        print("|2")
+
+    if a == "S":
+        print("5")
+    if a == "T":
+        print("7")
+    if a == "u":
+        print("(_) or |_|")
+    if a == "v":
+        print("\/")
+    if a == "W":
+        print("\/\/")
+    if a == "x":
+        print("}{ or )(")
+    if a == "y":
+        print("`/")
+    if a == "z":
+        print("2")
+    ag()
+
+def ag():
+    sus = input('want a anouther letter logan?: ')
+    if sus == "ya":
+        let()
+    else:
+        print("ok see ya")
+        time.sleep(1)
+
+    
+
+let()
+
+
+
+
+
+
